6b14969abeaa16f6bcc0566cc30e0a3d989abb07
karkiayush <karkiayush619@gmail.com>
2022-10-20 22:39:14 +0545

    whatsapp automation where once the message will be send at 22(i.e 10 at night) at 10:10 pm at night

--- whatsappAutomation.py
+++ whatsappAutomation.py
@@ -1,2 +1,4 @@
+from shutil import which
 import pywhatkit
-pywhatkit.sendwhatmsg('+9779842263181',"Hi there ! I'm Aayush",22,9)
+# the last  element of the below line of the code is time, which is counted from the mid night as 0:0
+pywhatkit.sendwhatmsg('+9779842263181',"Hi there ! I'm Aayush",22,10)
