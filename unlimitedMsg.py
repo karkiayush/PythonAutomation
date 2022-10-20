dc96a1b263c09e0d0d2a949eb0f7cee3deff732b
karkiayush <karkiayush619@gmail.com>
2022-10-20 22:36:35 +0545

    first commit

---
+++ unlimitedMsg.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+while True:
+    time.sleep(1
+               )
+    pyautogui.typewrite('Aayush vai lai kosheli lanu parxa hai hamle')
+    # time.sleep(3)
+    pyautogui.press('enter')
---
+++ whatsappAutomation.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+9779842263181',"Hi there ! I'm Aayush",22,9)
